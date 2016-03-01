0a1ec86312a50782ba9e86f8d2db2190286dc7d4
Iurii Tverezovskyi <Iurii Tverezovskyi>
2016-03-01 10:50:53 -0600

    TimeObject

---
+++ TimeMachine/src/com/gwexhibits/timemachine/objects/TimeObject.java
@@ -0,0 +1,105 @@
+package com.gwexhibits.timemachine.objects;
+
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.salesforce.androidsdk.smartstore.store.IndexSpec;
+import com.salesforce.androidsdk.smartstore.store.SmartStore;
+import com.salesforce.androidsdk.smartsync.app.SmartSyncSDKManager;
+import com.salesforce.androidsdk.smartsync.manager.SyncManager;
+import com.salesforce.androidsdk.smartsync.model.SalesforceObject;
+import com.salesforce.androidsdk.smartsync.util.Constants;
+
+import org.json.JSONObject;
+
+/**
+ * Created by psyfu on 3/1/2016.
+ */
+public class TimeObject extends SalesforceObject {
+
+    public static final String TIME_SUPE = "Times";
+    public static final String TIME_SF_OBJECT = "Times__c";
+
+    public static final String ORDER = "order__c";
+    public static final String NOTE = "note__c";
+    public static final String START_TIME = "startTime__c";
+    public static final String END_TIME = "endTime__c";
+    public static final String LOCAL = "__local__";
+
+
+    public static IndexSpec[] TIMES_INDEX_SPEC = {
+            new IndexSpec("Id", SmartStore.Type.string),
+            new IndexSpec(ORDER, SmartStore.Type.string),
+            new IndexSpec(NOTE, SmartStore.Type.string),
+            new IndexSpec(START_TIME, SmartStore.Type.string),
+            new IndexSpec(END_TIME, SmartStore.Type.string),
+            new IndexSpec(LOCAL, SmartStore.Type.string)
+    };
+
+    public static final String[] TIME_FIELDS_SYNC_UP = {
+            ORDER,
+            NOTE,
+            START_TIME,
+            END_TIME,
+    };
+
+    public static final String[] TIME_FIELDS_UPDATE = {
+            NOTE,
+            START_TIME,
+            END_TIME,
+    };
+
+    public static final String[] TIME_FIELDS_SYNC_DOWN = {
+            Constants.ID,
+            ORDER,
+            NOTE,
+            START_TIME,
+            END_TIME,
+            Constants.LAST_MODIFIED_DATE
+    };
+
+    private boolean isLocallyModified;
+
+
+    /**
+     * Parameterized constructor.
+     *
+     * @param object Raw data for object.
+     */
+    public TimeObject(JSONObject object) {
+        super(object);
+
+
+        objectType = "TimeObject";
+        objectId = object.optString(Constants.ID);
+        name = object.optString(ORDER) + " " + object.optString(Constants.ID) ;
+
+        isLocallyModified = object.optBoolean(SyncManager.LOCALLY_UPDATED) ||
+                object.optBoolean(SyncManager.LOCALLY_CREATED) ||
+                object.optBoolean(SyncManager.LOCALLY_DELETED);
+    }
+
+    /**
+     * Returns whether the times has been locally modified or not.
+     *
+     * @return True - if the contact has been locally modified, False - otherwise.
+     */
+    public boolean isLocallyModified() {
+        return isLocallyModified;
+    }
+
+    private String sanitizeText(String text) {
+        if (TextUtils.isEmpty(text) || text.equals(Constants.NULL_STRING)) {
+            return Constants.EMPTY_STRING;
+        }
+        return text;
+    }
+
+    public static String buildWhereRequest(){
+        Log.d("TIME OBJECT", SmartSyncSDKManager.getInstance().getUserAccountManager().getCurrentUser().getUserId());
+        return "CreatedById='" +
+                SmartSyncSDKManager.getInstance().getUserAccountManager().getCurrentUser().getUserId() +
+                "'";
+    }
+}
