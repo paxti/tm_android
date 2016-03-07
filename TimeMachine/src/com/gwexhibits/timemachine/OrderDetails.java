26084a1fad1f59dc92a7d481e62b3bfa03112f0b
Iurii Tverezovskyi <Iurii Tverezovskyi>
2016-03-07 08:37:49 -0600

    Move adapter to Adapter package

--- TimeMachine/src/com/gwexhibits/timemachine/OrderDetails.java
+++ TimeMachine/src/com/gwexhibits/timemachine/OrderDetails.java
@@ -3,11 +3,9 @@ package com.gwexhibits.timemachine;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.Preference;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.RecyclerView;
@@ -15,6 +13,7 @@ import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
 import android.widget.TextView;
 
+import com.gwexhibits.timemachine.adapters.OrderDetailsAdapter;
 import com.gwexhibits.timemachine.objects.OrderObject;
 import com.gwexhibits.timemachine.objects.TimeObject;
 
